547b8ac5335f0f611f8f7cdcd38e0a31008c6c57
obelchakva <obelchakva@gmail.com>
2025-03-21 13:04:12 +0300

    Added assignment3.cpp

--- Homework4/assignment3.cpp
+++ Homework4/assignment3.cpp
@@ -1,4 +1,4 @@
-/* Максимальный элемент
+/* Количество различных элементов
    1 1 1 1 2 2 2 3 3 3 3 3 -> 3 */
 
 #include <iostream>
@@ -7,5 +7,15 @@ using namespace std;
 
 int main()
 {
-
+	int a[12];
+	for(int i =0; i < 12; i++) {
+		cin >> a[i];
+	}
+	int count = 0;
+	for(int i = 0; i <12; i++) {
+		if(a[i+1] != a[i]) {
+			count += 1;
+		}
+	}
+	cout << count << endl;
 }
