4c7a6c9f8ab0ac333fb03ee3a4cbb05119f248aa
obelchakva <obelchakva@gmail.com>
2025-03-26 21:33:16 +0300

    assignment3 was updated

--- Homework4/assignment3.cpp
+++ Homework4/assignment3.cpp
@@ -1,4 +1,4 @@
-/* Количество различных элементов
+/* Максимальный промежуток постоянства
    1 1 1 1 2 2 2 3 3 3 3 3 -> 3 */
 
 #include <iostream>
@@ -11,11 +11,18 @@ int main()
 	for(int i =0; i < 12; i++) {
 		cin >> a[i];
 	}
-	int count = 0;
-	for(int i = 0; i <12; i++) {
-		if(a[i+1] != a[i]) {
-			count += 1;
+	int count = 1;
+	int max_count;
+	for(int i = 0; i < 11; i++) {
+		if(a[i] == a[i+1]) {
+			count +=1;
+		} 
+		else {
+			if(count > max_count) {
+				max_count = count;
+			}
+			count = 1;
 		}
 	}
-	cout << count << endl;
+	cout << max_count << endl;
 }
